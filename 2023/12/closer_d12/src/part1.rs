45ac1c3ad8a4b0b92446219907cdb5a8a7902f00
Kenneth Lo <closer.tw@gmail.com>
2024-01-12 21:33:30 +0800

    2023 Day 12 Part 2 Solved

--- 2023/12/closer_d12/src/part1.rs
+++ 2023/12/closer_d12/src/part1.rs
@@ -22,7 +22,7 @@ pub fn solve(input: &Vec<&str>) -> u64 {
         .sum()
 }
 
-fn solve_line(springs: &[char], sequences: &[usize]) -> usize {
+pub fn solve_line(springs: &[char], sequences: &[usize]) -> usize {
     if sequences.is_empty() {
         if springs.contains(&'#') {
             0
@@ -30,110 +30,15 @@ fn solve_line(springs: &[char], sequences: &[usize]) -> usize {
             1
         }
     } else {
-        if let Some((start, end)) = find_test_range(springs) {
-            let length = sequences[0];
-            let rests = &sequences[1..];
-            (start..=end)
-                .filter(|idx| can_fit(&springs[*idx..], length))
-                .map(|idx| solve_line(&springs[(idx + length + 1)..], rests))
-                .sum()
-        } else {
-            0
-        }
-    }
-}
-
-fn find_test_range(springs: &[char]) -> Option<(usize, usize)> {
-    let start = springs
-        .iter()
-        .enumerate()
-        .find(|(_, c)| **c != '.')
-        .map(|(idx, _)| idx);
-    let end = start
-        .map(|s| {
-            springs
-                .iter()
-                .skip(s)
-                .enumerate()
-                .find(|(_, c)| **c == '#')
-                .map(|(idx, _)| idx + s)
-        })
-        .flatten();
-
-    match (start, end) {
-        (Some(s), Some(e)) => Some((s, e)),
-        (Some(s), None) => Some((s, springs.len() - 1)),
-        _ => None,
-    }
-}
-
-fn can_fit(cs: &[char], length: usize) -> bool {
-    cs.len() > length && cs.iter().take(length).map(|&c| c != '.').all(|b| b) && cs[length] != '#'
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_find_range() {
-        let input = [
-            "???.###",
-            ".??..??...?##.",
-            "?#?#?#?#?#?#?#?",
-            "????.#...#...",
-            "????.######..#####.",
-            "?###????????",
-            "....???#....",
-        ]
-        .into_iter()
-        .map(|s| s.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-        assert_eq!(find_test_range(&input[0]), Some((0, 4)));
-        assert_eq!(find_test_range(&input[1]), Some((1, 11)));
-        assert_eq!(find_test_range(&input[2]), Some((0, 1)));
-        assert_eq!(find_test_range(&input[3]), Some((0, 5)));
-        assert_eq!(find_test_range(&input[4]), Some((0, 5)));
-        assert_eq!(find_test_range(&input[5]), Some((0, 1)));
-        assert_eq!(find_test_range(&input[6]), Some((4, 7)));
-    }
-
-    #[test]
-    fn test_can_fit() {
-        let cs: Vec<_> = "?###????????".chars().collect();
-        assert_eq!(can_fit(&cs[0..], 3), false);
-        assert_eq!(can_fit(&cs[1..], 3), true);
-    }
-
-    #[test]
-    fn test_lines() {
-        let springs_vec = [
-            "???.###",
-            ".??..??...?##.",
-            "?#?#?#?#?#?#?#?",
-            "????.#...#...",
-            "????.######..#####.",
-            "?###????????",
-        ]
-        .into_iter()
-        .map(|s| s.chars().chain(['.'].into_iter()).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-        let sequences_vec = vec![
-            vec![1, 1, 3],
-            vec![1, 1, 3],
-            vec![1, 3, 1, 6],
-            vec![4, 1, 1],
-            vec![1, 6, 5],
-            vec![3, 2, 1],
-        ];
-
-        assert_eq!(solve_line(&springs_vec[0], &sequences_vec[0]), 1);
-        assert_eq!(solve_line(&springs_vec[1], &sequences_vec[1]), 4);
-        assert_eq!(solve_line(&springs_vec[2], &sequences_vec[2]), 1);
-        assert_eq!(solve_line(&springs_vec[3], &sequences_vec[3]), 1);
-        assert_eq!(solve_line(&springs_vec[4], &sequences_vec[4]), 4);
-        assert_eq!(solve_line(&springs_vec[5], &sequences_vec[5]), 10);
+        let end = springs
+            .iter()
+            .enumerate()
+            .find(|(_, c)| **c == '#')
+            .unwrap_or((springs.len(), &'_'))
+            .0;
+        (0..=end)
+            .filter(|idx| closer_d12::can_fit(&springs[*idx..], sequences[0]))
+            .map(|idx| solve_line(&springs[(idx + sequences[0] + 1)..], &sequences[1..]))
+            .sum()
     }
 }
--- 2023/12/closer_d12/src/part2.rs
+++ 2023/12/closer_d12/src/part2.rs
@@ -1,3 +1,84 @@
+use std::cell::RefCell;
+use std::{collections::HashMap, iter};
+
 pub fn solve(input: &Vec<&str>) -> u64 {
-    0
+    input
+        .iter()
+        .map(|line| {
+            let mut parts = line.split(' ');
+            let springs_vec = parts.next().unwrap().chars().collect::<Vec<_>>();
+            let springs = iter::repeat(springs_vec)
+                .take(5)
+                .collect::<Vec<_>>()
+                .join(&'?')
+                .into_iter()
+                .chain(['.'].into_iter())
+                .collect::<Vec<_>>();
+
+            let sequences_vec = parts
+                .next()
+                .unwrap()
+                .split(',')
+                .map(|token| token.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            let sequences = iter::repeat(sequences_vec)
+                .take(5)
+                .collect::<Vec<_>>()
+                .join(&[][..]);
+
+            let mut memory = RefCell::new(HashMap::new());
+            solve_line_caches(&springs, &sequences, &mut memory) as u64
+        })
+        .sum()
+}
+
+pub fn solve_line_caches<'a>(
+    springs: &'a [char],
+    sequences: &'a [usize],
+    memory: &RefCell<HashMap<(Vec<char>, Vec<usize>), usize>>,
+) -> usize {
+    if sequences.is_empty() {
+        if springs.contains(&'#') {
+            0
+        } else {
+            1
+        }
+    } else {
+        let end = springs
+            .iter()
+            .enumerate()
+            .find(|(_, c)| **c == '#')
+            .unwrap_or((springs.len(), &'_'))
+            .0;
+
+        let mut sum = 0;
+
+        (0..=end)
+            .filter(|idx| closer_d12::can_fit(&springs[*idx..], sequences[0]))
+            .for_each(|idx| {
+                let new_springs = springs[(idx + sequences[0] + 1)..].to_vec();
+                let new_sequence = sequences[1..].to_vec();
+                let c = memory
+                    .borrow()
+                    .get(&(new_springs.clone(), new_sequence.clone()))
+                    .map(|v| *v);
+                let x = match c {
+                    Some(value) => value,
+                    None => {
+                        let value = solve_line_caches(
+                            &springs[(idx + sequences[0] + 1)..],
+                            &sequences[1..],
+                            memory,
+                        );
+                        memory
+                            .borrow_mut()
+                            .insert((new_springs, new_sequence), value);
+                        value
+                    }
+                };
+                sum += x;
+            });
+
+        sum
+    }
 }
